d2702b9911662f75b82a2fc60e140dfad0f945b0
Bastian Boll <mail@bbboll.com>
2018-02-21 18:06:08 +0100

    began mining

--- mine_subtitles.py
+++ mine_subtitles.py
@@ -1,1 +1,17 @@
-# TODO
+import csv
+
+talks = []
+
+with open('data/kaggle/ted_main.csv', 'rb') as csvfile:
+	csvreader = csv.reader(csvfile, delimiter=',')
+	for row in csvreader:
+		if row[7] == 'name':
+			continue
+		url = row[15]
+		if url.endswith('\n'):
+			url = url[0:len(url)-1]
+		talks.append({
+			"title": row[14],
+			"duration": row[2],
+			"url": url})
+print(talks[135])
