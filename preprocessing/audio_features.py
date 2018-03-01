d154f7f7893ca13d68d646a0fd614fcb18def8e5
Paul Warkentin <paul@warkentin.email>
2018-03-01 19:20:41 +0100

    audio features extraction

---
+++ preprocessing/audio_features.py
@@ -0,0 +1,71 @@
+import numpy as np
+import os.path
+import scipy.io.wavfile as wav
+from python_speech_features import mfcc
+from python_speech_features import delta
+from python_speech_features import logfbank
+
+
+class AudioFeatures(object):
+	"""Objects of this handle extracting and loading audio features from files.
+
+	Attributes:
+		features (obj:`numpy.ndarray`): NumPy array holding audio features. Default is None.
+	"""
+
+	def __init__(self):
+		"""Initialize a new empty object.
+		"""
+		self.features = None
+
+	def load_from_wav(self, path):
+		"""Load audio features from existing wav file.
+
+		Arguments:
+			path (str): Path to an existing wav file.
+		"""
+		if not os.path.isfile(path):
+			print("Please download audio first.")
+			exit()
+
+		# load wav audio data
+		(rate, sig) = wav.read(path)
+		self.features = mfcc(sig,rate)
+
+	def load_from_numpy(self, path):
+		"""Load extracted audio features from existing numpy file.
+
+		Arguments:
+			path (str): Path to an existing numpy file.
+		"""
+		if not os.path.isfile(path):
+			print("Please download audio first.")
+			exit()
+
+		# load audio features
+		self.features = np.load(path)
+
+	def save_to_numpy(self, path):
+		"""Save extracted features to a numpy file.
+
+		Arguments:
+			path (str): Path to save features to.
+		"""
+		if self.features is None:
+			print("Please extract features first.")
+			exit()
+
+		# save features
+		np.save(path, self.features)
+
+
+if __name__ == "__main__":
+	"""Testing the audio features extraction.
+	"""
+
+	audio = AudioFeatures()
+	audio.load_from_wav("audio_features_example/english.wav") # https://raw.githubusercontent.com/jameslyons/python_speech_features/master/english.wav
+	audio.save_to_numpy("audio_features_example/english.npy")
+
+	print(audio.features.shape)
+	print(audio.features[:3, :])
