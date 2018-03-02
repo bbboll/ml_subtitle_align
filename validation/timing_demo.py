4c20d40f650f72914861b97bbeb13cec51f6d61a
Bastian Boll <bastibboll@googlemail.com>
2018-03-02 16:18:06 +0100

    setup for visual validation concept

---
+++ validation/timing_demo.py
@@ -0,0 +1,21 @@
+
+class TimingDemo(object):
+	"""
+	Provides functionality to play an audio file while 
+	concurrently displaying associated subtitles.
+	"""
+	def __init__(self, wav_filename, subtitle):
+		self.wav_filename = wav_filename
+		self.subtitle = subtitle
+
+	def play(self):
+		"""
+		Plays the wav and concurrently displays subtitles
+		"""
+		pass
+
+	def play_wav(self):
+		pass
+
+	def display_subtitles(self):
+		pass
