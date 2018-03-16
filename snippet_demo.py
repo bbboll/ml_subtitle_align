eaed8345ae5f2ceefb20aa5983d11f84faadf6dd
Bastian Boll <mail@bbboll.com>
2018-03-16 18:41:26 +0100

    snippet demo

---
+++ snippet_demo.py
@@ -0,0 +1,51 @@
+from model.model import Model
+import os.path
+import json
+import tensorflow as tf
+import extract_training_data as extractor
+import numpy as np
+
+def _path(relpath):
+	"""
+	Returns an absolute path for the given path (which is relative to the root directory ml_subtitle_align)
+	"""
+	current_dir = os.path.dirname(__file__)
+	return os.path.abspath(os.path.join(current_dir, relpath))
+
+if not os.path.isfile(extractor.frequent_words_path) or not os.path.isfile(extractor.word_timings_path):
+	print("Execute extract_training_data first.")
+	exit()
+frequent_words = json.load(open(extractor.frequent_words_path))
+word_timings = json.load(open(extractor.word_timings_path))
+mfcc_per_interval = int(extractor.INTERVAL_SIZE / 0.005)
+
+if __name__ == '__main__':
+
+	# start a new tensorflow session
+	sess = tf.InteractiveSession()
+
+	# load model
+	model_load_checkpoint = _path("pretrained_models/0316/model.ckpt-5")
+	input_3d = tf.placeholder(tf.float32, [None, 80, 13], name="input_3d")
+	model = Model()
+	prediction = model.test_model(input_3d)
+	model.load_variables_from_checkpoint(sess, model_load_checkpoint)
+
+	# load input
+	mfcc_features = np.load(_path("snippet_demo/snippet.npy"))
+	mfcc_features = mfcc_features.reshape((1,80,13))
+
+	val_prediction = sess.run(
+		[prediction],
+		feed_dict={
+			input_3d: mfcc_features
+		}
+	)
+	val_prediction = np.array(val_prediction).reshape((1500,))
+	n = 10
+	idx = (-val_prediction).argsort()[:n].astype(int)
+	print(" --- The computed {} most likely words in the snippet are --- ".format(n))
+	for i in idx:
+		print("{} - {}".format(val_prediction[i], frequent_words[i]))
+
+
