bbeeced0b186d9a5905da42d26e3f565bbaa52aa
Paul Warkentin <paul@warkentin.email>
2018-03-20 14:52:25 +0100

    updated conv lstm

--- models/conv_lstm_model.py
+++ models/conv_lstm_model.py
@@ -12,6 +12,9 @@ class Model(BaseModel):
 		The embedding of conv LSTM output into the label space
 		is performed by two dense layers.
 		"""
+		time_seq_len = 1
+		output_cannels = 1
+
 		if is_training:
 			# load hyperparameter for dropout layers
 			# lower keep probability can be used to control overfitting
@@ -19,13 +22,13 @@ class Model(BaseModel):
 
 		# prepare input for convolution
 		input_shape = input_3d.get_shape()
-		input_4d = tf.reshape(input_3d, [-1, 10, int(input_shape[1]), int(input_shape[2])])
+		input_4d = tf.reshape(input_3d, [-1, time_seq_len, int(input_shape[1]), int(input_shape[2])])
 		input_4d = tf.expand_dims(input_4d, -1)
 
 		# convolutional lstm
 		conv_lstm_cell = tf.contrib.rnn.Conv2DLSTMCell(input_shape=[int(input_shape[1]), int(input_shape[2]), 1],
 													   kernel_shape=[3, 3],
-													   output_channels=1)
+													   output_channels=output_cannels)
 		conv_lstm_output, conv_lstm_state = tf.nn.dynamic_rnn(conv_lstm_cell,
 															  input_4d,
 															  time_major=False,
